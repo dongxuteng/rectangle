9f7e43eb35c8d6afb7b49bf79d37be1af8194f81
wangding <408542507@qq.com>
2019-04-02 15:32:58 +0800

    fild h5 validation

--- rectangle.js
+++ rectangle.js
@@ -1,79 +1,29 @@
 $(function() {
       var $width = $('#width'),
         $height = $('#height'),
-        $btnCal = $('#calculate'),
+        $form = $('form'),
         $perimeter = $('#perimeter'),
         $area = $('#area');
-        $widthValidation = $('#width-validation');
-        $heightValidation = $('#height-validation');
-
-
-
-function Rectangle(width, height) {
-    var w = Number(width),
+function Rectangle(width,height){
+  var w = Number(width),
       h = Number(height);
-
-this.area = function() {
-      return w * h;
-        
-};
-
-this.perimeter = function() {
-      return 2 * (w + h);
-        
-};
+  this.perimeter = function(){
+    return 2*(w+h);
+  };
+  this.area = function(){
+    return w*h;
+  };
 
 }
 
-$width.focusout(function(){
-  var w = $width.val();
-   if(w === ''){
-      $widthValidation.html('密码不能为空!');
-      $width.select();
-   }else{
-     $widthValidation.html('');
-   }
-
-   if(Number(w)<0){
-     $widthValidation.html('宽度不能为空');
-     $width.select();
-   }else{
-     $heightValidation.html('高度不能为空');
-   }
-
-});
-
-$height.focusout(function(){
-  var h = $height.val();
-  if(h === ''){
-    $heightValidation.html('高度不能为空');
-    $height.select();
-  }else{
-    $heightValidation.html('');
-  }
-
- if(Number(w)<0){
-     $heightValidation.html('高度不能为空');
-     $height.select();
- }else{
-   $heightValidation.html('');
- }
-})
 
 
-$btnCal.click(function(){
+$form.submit(function(e){
+        e.preventDefault();
         var w = $width.val(),
             h = Number($height.val());       
        
        var r = new Rectangle(w, h);
-        if(w === ''){
-           $widthValidation.html('密码不能为空!');
-           $width.select();
-        }
-        if(h === ''){
-          $heightValidation.html('高度不能为空');
-          $height.select();
-        }
 
 
        $perimeter.val(r.perimeter());
